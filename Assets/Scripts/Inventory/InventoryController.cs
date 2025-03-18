4cabe960437f18ff4edc3f4729038e6dd0ccf781
Saif Ullah <ullahs3@udayton.edu>
2025-03-17 22:30:22 -0400

    updated inventory mechanic

---
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject inventoryPanel;
+    public GameObject slotPrefab;
+    public int slotCount;
+    public GameObject[] itemPrefabs;
+    void Start()
+    {
+        for (int i = 0; i < slotCount; i++)
+        {
+            Slot slot = Instantiate(slotPrefab, inventoryPanel.transform).GetComponent<Slot>();
+            if (i < itemPrefabs.Length)
+            {
+                GameObject item = Instantiate(itemPrefabs[i], slot.transform);
+                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = item;
+            }
+        }
+    }
+
+    public bool AddItem(GameObject itemPrefab)
+    {
+        foreach (Transform slotTransform in inventoryPanel.transform)
+        {
+            Slot slot = slotTransform.GetComponent<Slot>();
+            if (slot != null && slot.currentItem == null)
+            {
+                GameObject newItem = Instantiate(itemPrefab, slot.transform);
+                newItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                slot.currentItem = newItem;
+                return true;
+            }
+        }
+        Debug.Log("Inventory is full!");
+        return false;
+    }
+}
---
+++ Assets/Scripts/Inventory/ItemDragHandler.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    Transform originalParent;
+    CanvasGroup canvasGroup;
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalParent = transform.parent; //save og parent
+        transform.SetParent(transform.root); // above other canvas
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.alpha = 0.6f; // semi transparent during drag
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position; //follow mouse
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = true; //enables raycast
+        canvasGroup.alpha = 1f;
+
+        Slot dropSlot = eventData.pointerEnter?.GetComponent<Slot>();
+
+        if (dropSlot == null)
+        {
+            GameObject item = eventData.pointerEnter;
+            if (item != null)
+            {
+                dropSlot = item.GetComponentInParent<Slot>();
+            }
+        }
+        Slot originalSlot = originalParent.GetComponent<Slot>();
+
+        if (dropSlot != null)
+        {
+            if (dropSlot.currentItem != null)
+            {
+                //slot has items - swap
+                dropSlot.currentItem.transform.SetParent(originalSlot.transform);
+                originalSlot.currentItem = dropSlot.currentItem;
+                dropSlot.currentItem.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+            else
+            {
+                originalSlot.currentItem = null;
+            }
+            //move item into drop slot
+            transform.SetParent(dropSlot.transform);
+            dropSlot.currentItem = gameObject;
+        }
+        else
+        {
+            //no slot under drop point
+            transform.SetParent(originalParent);
+        }
+        GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+    }
+
+}
