37ba16b3045e3f52174ab52a6752f1cfa89e2b8b
patels44 <patels44@udayton.edu>
2025-04-15 10:55:54 -0400

    Timer script updated

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timeRemaining = 180f; // 3 minutes in seconds
+    public TextMeshProUGUI timerText;
+    private bool timerIsRunning = true;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                UpdateTimerDisplay();
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+                UpdateTimerDisplay();
+                Debug.Log("Time's up!");
+            }
+        }
+    }
+
+    void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60);
+        int seconds = Mathf.FloorToInt(timeRemaining % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
