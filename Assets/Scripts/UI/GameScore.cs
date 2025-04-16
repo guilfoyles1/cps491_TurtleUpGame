ead3d771a15c0c1f2ce489344aefc8ba23142900
Saif Ullah <ullahs3@udayton.edu>
2025-04-15 23:10:01 -0400

    added gamescore coloring

--- Assets/Scripts/UI/GameScore.cs
+++ Assets/Scripts/UI/GameScore.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 
@@ -5,16 +6,22 @@ public class GameScore : MonoBehaviour
 {
     public int score = 0;
     public TextMeshProUGUI scoreText;
+    private Coroutine flashRoutine;
+    private Material mat;
+    private Color defaultOutlineColor;
 
     void Start()
     {
         UpdateScoreUI();
+        mat = scoreText.fontMaterial;
+        defaultOutlineColor = mat.GetColor(ShaderUtilities.ID_OutlineColor);
     }
 
     public void AddScore(int amount)
     {
         score += amount;
         UpdateScoreUI();
+        FlashOutline(Color.green);
     }
 
     public void SubtractScore(int amount)
@@ -22,6 +29,7 @@ public class GameScore : MonoBehaviour
         score -= amount;
         score = Mathf.Max(score, 0);
         UpdateScoreUI();
+        FlashOutline(Color.red);
     }
 
     private void UpdateScoreUI()
@@ -30,5 +38,23 @@ public class GameScore : MonoBehaviour
         {
             scoreText.text = score.ToString();
         }
+    }
+
+    public void FlashOutline(Color flashColor)
+    {
+        if (flashRoutine != null)
+        {
+            StopCoroutine(flashRoutine);
+            // Immediately reset to default in case coroutine didn't finish
+            mat.SetColor(ShaderUtilities.ID_OutlineColor, defaultOutlineColor);
+        }
+        flashRoutine = StartCoroutine(FlashOutlineCoroutine(flashColor));
+    }
+
+    private IEnumerator FlashOutlineCoroutine(Color flashColor)
+    {
+        mat.SetColor(ShaderUtilities.ID_OutlineColor, flashColor);
+        yield return new WaitForSeconds(0.7f);
+        mat.SetColor(ShaderUtilities.ID_OutlineColor, defaultOutlineColor);
     }
 }
